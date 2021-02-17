84cf3fd49e315928211d52188701a8e666959641
balajikhanponnambalam <vpbalajimca@gmail.com>
2021-02-17 13:57:02 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- after application loading into the DOM mount cart.vue -->
+    <cart />
+  </div>
+</template>
+
+<script>
+import Cart from "./components/cart/Cart";
+export default {
+  name: "app",
+  components: {
+    Cart
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
